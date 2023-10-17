490d1393cd5755944bcb21ce738b9e1d1f74e76a
BernardoGuerino <bernardo.coelho.guerino@gmail.com>
2023-10-17 20:17:10 -0300

    Update imprimircsv.py

--- imprimircsv.py
+++ imprimircsv.py
@@ -1,18 +1,19 @@
+def ler_arquivo_csv () :
+    with open(arquivo_csv, 'r', newline='') as arquivo_csv:
+        leitor_csv = csv.reader(arquivo_csv)
+
+        except FileNotFoundError:
+            print(f'O arquivo {arquivo_csv} não foi encontrado.')
+    
 def imprimir_csv(arquivo_csv):
-    try:
-        with open(arquivo_csv, 'r', newline='') as arquivo_csv:
-            leitor_csv = csv.reader(arquivo_csv)
-            
-            for linha in leitor_csv:
-                imp1=(linha['Valor']*imp1)/100
-                imp2=(linha['Valor']*imp2)/100
-                imp3=(linha['Valor']*imp3)/100
-                frete=(linha['Frete']/qnt)
-                valor_final=valor+imp1+imp2+imp3+frete
-                venda=valor_final+(valor_final*(ganho/100))    
-                print(linha)
+    ler_arquivo_csv()
+    for linha in leitor_csv:
+        imp1=(linha['Valor']*imp1)/100
+        imp2=(linha['Valor']*imp2)/100
+        imp3=(linha['Valor']*imp3)/100
+        frete=(linha['Frete']/qnt)
+        valor_final=valor+imp1+imp2+imp3+frete
+        venda=valor_final+(valor_final*(ganho/100))    
+        print(linha)
            
-    except FileNotFoundError:
-        print(f'O arquivo {arquivo_csv} não foi encontrado.')
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+   
